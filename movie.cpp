4333bf6ca6198084eb4ba8f2550fd4a61b38936c
Diksha <diksha@macbookpro>
2025-09-26 18:32:20 -0700

    getters/setters/instance variables

--- movie.cpp
+++ movie.cpp
@@ -6,3 +6,34 @@
 #include <string>
 #include <iomanip>
 using namespace std;
+
+
+class Movie{
+private:
+    string writer;	//movie's screenwriter
+    int year;		//year movie was released
+    string title;	//movie's title
+
+public:
+    // getters: no arguments, returns the stored values of the instance variables
+    int getWriter()		{ return writer; }
+    int getYear()		{ return year; }
+    int getTitle()		{ return title; }
+
+    // setters: take ints as arguments and sets instance variables
+    void setWriter(string w)	{ writer = w; }
+    void setYear(int y)		{ year = y; }
+    void setTitle(string t)	{ title = t; }
+
+};
+
+int main(){
+
+    Movie kk;
+    kk.setWriter("Robert Mark Kamen");
+    kk.setYear(1984);
+    kk.setTitle("The Karate Kid");
+
+
+
+}
